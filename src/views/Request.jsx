39c519210ecf89994a722e527f4a27ffe09f2104
胡贵山 <13450737557@163.com>
2019-04-18 10:14:15 +0800

    update

--- src/views/Request.jsx
+++ src/views/Request.jsx
@@ -1,9 +1,12 @@
-
-<template>
-    <p @click="click">request</p>
-</template>
-<script>
+import style from "../assets/css/test.css"
 export default {
+    created() {
+    },
+    render() {
+        return (
+            <div onClick={this.click} class={style.test}>jsx</div>
+        )
+    },
     methods: {
         async click() {
             let response = await this.$axios.get("https://www.easy-mock.com/mock/5c46cd90eaf31779b928ae07/example/mock")
@@ -11,10 +14,3 @@ export default {
         }
     }
 }
-</script>
-<style lang="scss" scoped>
-    .request {
-        background: #FFCCCC;
-        height: 98vh;
-    }
-</style>
